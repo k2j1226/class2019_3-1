1aa44b1ef3ad09928fe2da31027609b6816f5630
k2j1226 <k5j1226@naver.com>
2019-09-05 16:13:05 +0900

    test

---
+++ WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Unit.java
@@ -0,0 +1,15 @@
+package kr.ac.daelim.uml;
+
+public class Unit {
+	public String name;
+	public int health;
+	public void move()
+	{
+		System.out.println("유닛이 이동한다");
+	}
+	public void UnderAttack(Charactor charactor)
+	{
+		System.out.println("유닛이 공격받는다");
+	}
+
+}
