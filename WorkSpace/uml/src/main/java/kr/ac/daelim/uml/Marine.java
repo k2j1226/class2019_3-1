577576f063d0c29c56d35698346969fbac916416
k2j1226 <k5j1226@naver.com>
2019-09-05 16:42:43 +0900

    d

--- WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
+++ WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
@@ -1,5 +1,22 @@
 package kr.ac.daelim.uml;
 
-public class Marine {
+public class Marine extends Unit {
 
+	protected int AttackPower;
+	
+	public void Move()
+	{
+		System.out.println("마린이 이동한다");
+	}
+	
+	public void Attack()
+	{
+		System.out.println("마린이 공격한다");
+	}
+	
+    public void UnderAttack(Charactor charactor)
+    {
+    	System.out.println("마린이 공격받는다");
+    }
+	
 }
--- WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Medic.java
+++ WorkSpace/uml/src/main/java/kr/ac/daelim/uml/Medic.java
@@ -1,5 +1,20 @@
 package kr.ac.daelim.uml;
 
-public class Medic {
+public class Medic extends Unit {
+	
+	public void Move()
+	{
+		System.out.println("메딕이 이동한다");
+	}
+	
+	public void UnderAttack(Charactor charactor)
+	{
+		System.out.println("메딕이 공격받는다");
+	}
+	
+	public void Healing(Charactor charactor)
+	{
+		System.out.println("메딕이 힐링한다");
+	}
 
 }
